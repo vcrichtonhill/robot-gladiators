869ff12ee8b55e1f63b11a62c00d79422537796a
Vee Crichton-Hill <vcrichtonhill@gmail.com>
2022-03-16 16:24:08 -0500

    battle sequence made

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,10 +8,11 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    window.alert("Welcome to Robot Gladiators!");
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+  // as long as the enemy is alive
+  while(enemyHealth > 0) {
+     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
-  if (promptFight === "fight" || promptFight === "FIGHT") {
+    if (promptFight === "fight" || promptFight === "FIGHT") {
     
     // attack enemy
     enemyHealth = enemyHealth - playerAttack;
@@ -61,9 +62,14 @@ var fight = function(enemyName) {
   }  
   else {
     window.alert("You need to choose a valid option. Try again!")
+    }
   }
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
-}
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  fight(pickedEnemyName);
+} 
+
+// fight();
