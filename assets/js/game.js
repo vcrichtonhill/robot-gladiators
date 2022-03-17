25ad9ec40f1733d6afb5c042506754fd630959b6
Vee Crichton-Hill <vcrichtonhill@gmail.com>
2022-03-16 23:06:03 -0500

    added shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -84,7 +84,18 @@ var startGame = function() {
 
       //use variable
       fight(pickedEnemyName);
-    }
+      
+      //if player is alive and we're not at the last enemy in the array
+      if (playerHealth > 0 && i < enemyNames.length - 1) {
+        // ask to visit shop
+        var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+
+        // if yes
+        if (storeConfirm) {   
+        shop();
+      }
+    } 
+  }
     else {
       window.alert("You have lost your robot in battle! Game Over!");
       break;
@@ -114,6 +125,55 @@ var endGame = function() {
   }
   else {
     window.alert("Thanks for playing!");
+  }
+};
+
+var shop = function() {
+  //ask player what they'd like to do
+  var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice.");
+
+  //switch to carry out action
+  switch (shopOptionPrompt) {
+    case "refill":
+    case "REFILL":
+      if (playerMoney >= 7) {
+      window.alert("Refilling player's health by 20 for 7 dollars.");
+
+      // increase health, decrease money
+      playerHealth = playerHealth + 20;
+      playerMoney = playerMoney - 7;
+    }
+      else {
+        window.alert("You don't have enough money!");
+      }
+      break;
+
+    case "upgrade":
+    case "UPGRADE":
+      if (playerMoney >= 7) {
+      window.alert("Upgrading player's attack by 6 for 7 dollars.");
+
+      //increase attack, decrease money
+      playerAttack = playerAttack + 6;
+      playerMoney = playerMoney - 7;
+      }
+      else {
+        window.alert("You don't have enough money!");
+      }
+      break;
+
+    case "leave":
+    case "LEAVE":
+      window.playerMoney("Leaving the store.");
+
+    //do nothing, function ends
+    break;
+
+    default:
+      window.alert("You did not pick a valid option. Try again.");
+      // call shop again
+      shop()
+      break;
   }
 };
 
